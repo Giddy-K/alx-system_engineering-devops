df8c9fb16aad1b8a5420ee187a3e648788b20477
Giddy-K <gideonkipamet@gmail.com>
2024-08-14 13:47:45 +0300

    Clean up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -26,13 +26,13 @@ def number_of_subscribers(subreddit):
     except requests.RequestException:
         return 0
 
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    # """Return the total number of subscribers on a given subreddit."""
+    # url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    # headers = {"User-Agent": "Mozilla/5.0"}
+    # response = requests.get(url, headers=headers, allow_redirects=False)
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     subscribers = data['data']['subscribers']
+    #     return subscribers
+    # else:
+    #     return 0
