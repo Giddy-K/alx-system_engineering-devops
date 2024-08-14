e65a94d642e2f64829a57e112ba6928f495e7ff5
Giddy-K <gideonkipamet@gmail.com>
2024-08-14 13:38:34 +0300

    Minor changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,35 +6,33 @@ Script that queries the number of subscribers on a given Reddit subreddit.
 import requests
 
 def number_of_subscribers(subreddit):
-<<<<<<< HEAD
-"""
+    """
     Return the total number of subscribers for a given subreddit.
     If the subreddit is invalid or does not exist, return 0.
-"""
-url = f"https://www.reddit.com/r/{subreddit}/about.json"
-headers = {"User-Agent": "Mozilla/5.0 (compatible; RedditAPI/0.1)"}
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; RedditAPI/0.1)"}
     
-try:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            return data['data'].get('subscribers', 0)
+        elif response.status_code == 404:
+            return 0
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data['data'].get('subscribers', 0)
-    elif response.status_code == 404:
-        return 0
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
-except requests.RequestException:
-    return 0
-
-=======
-"""Return the total number of subscribers on a given subreddit."""
-url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(url, headers=headers, allow_redirects=False)
-if response.status_code == 200:
-    data = response.json()
-    subscribers = data['data']['subscribers']
-    return subscribers
-else:
-    return 0
->>>>>>> a7106ab1a7daa18e931d679549b4b5d7b58a4fdb
